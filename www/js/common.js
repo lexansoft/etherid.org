e5918c88abb4192de745c3224e13e1524ffd596b
alexn <alexn@lexansoft.com>
2015-11-16 15:28:13 -0800

    no message

--- www/js/common.js
+++ www/js/common.js
@@ -1,4 +1,31 @@
+ETHERID_CONTRACT = "0xed476bae62d536e993e30faaaa9482b70ac35449"
+
+domains = new Array()
+ids = new Array()
+
 
 $().ready( function(e){ 
-    $( "#tabs" ).tabs();    
+    $( "#tabs" ).tabs(
+        {
+            beforeActivate: function( event, ui ) 
+            {
+                if( ui.newPanel[0].id != "tabs-info" ) 
+                {
+                    if( domains.length == 0 ) refresh_lists();
+                }
+            }    
+
+        }
+    );    
 } )
+
+
+function refresh_lists()
+{
+    $( "#stat" ).css('visibility', 'visible');
+    
+    
+    
+    
+}
+
