0e24dea844003eb0f518c14ab5979045c79c183c
alexn <alexn@lexansoft.com>
2016-02-10 14:20:24 -0800

    removed uglification because of the BigNumber confusion

--- b.bash
+++ b.bash
@@ -1,4 +1,6 @@
 #!/bin/sh
 # build the LLL contract
 
-browserify -r multihashes -r Buffer -r bs58 -r utf8 -r web3 -r etherid-js -r bignumber.js -r cookies-js -r progressbar.js -r sweetalert  | uglifyjs > www/js/bundle.js
+#browserify -r multihashes -r Buffer -r bs58 -r utf8 -r web3 -r etherid-js -r bignumber.js -r cookies-js -r progressbar.js -r sweetalert  | uglifyjs > www/js/bundle.js
+
+browserify -r multihashes -r Buffer -r bs58 -r utf8 -r web3 -r etherid-js -r bignumber.js -r cookies-js -r progressbar.js -r sweetalert  > www/js/bundle.js
